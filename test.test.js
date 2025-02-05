3d48d28826c7f018ae71bad9eed0268ad4b6d9d3
Marley Higbee <mhigbee1@emich.edu>
2025-02-05 13:23:33 -0500

    made test that fails to test if CI blocks merging

--- test.test.js
+++ test.test.js
@@ -1,4 +1,4 @@
-// This is a sample test to ensure that tests actually are run
+// This is a sample test to ensure that tests actually are run. This test should fail and block the merge.
 test('1=1', () => {
-    expect(1).toBe(1);
+    expect(1).toBe(42);
   });
