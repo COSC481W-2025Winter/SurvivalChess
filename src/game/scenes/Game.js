a100c6f2edb044702822e26c4352d333246e7867
Rituparna Ghosh <rghosh1@emich.edu>
2025-02-19 11:36:02 -0500

    resolved game.js

--- src/game/scenes/Game.js
+++ src/game/scenes/Game.js
@@ -1,34 +1,31 @@
 import { Scene } from "phaser";
 import { EventBus } from "../EventBus";
+
 import { SettingsButton } from "./SettingsButton";
 
-import {
-    PAWN,
-    ROOK,
-    KNIGHT,
-    BISHOP,
-    QUEEN,
-    KING,
-    CREAMHEX,
-    ONYXHEX,
-} from "../../game-objects/constants";
+import { RulesButton } from "./RulesButton";
+import { ChessTiles } from '../../game-objects/chess-tiles';
+
+
+import { PAWN, ROOK, KNIGHT, BISHOP, QUEEN, KING } from "../../game-objects/constants";
+import { CREAMHEX, ONYXHEX } from "../../game-objects/constants";
 import { PLAYER, COMPUTER } from "../../game-objects/constants";
-import { ChessTiles } from "../../game-objects/chess-tiles";
+
 export class Game extends Scene {
     constructor() {
         super("MainGame");
     }
     preload() {
         this.load.setPath("assets");
-        this.load.image("star", "star.png");
-        this.load.image("background", "bg.png");
         // Load Chess piece pngs
         this.load.setPath("assets/drummyfish chess");
         for (let rank of [PAWN, ROOK, KNIGHT, BISHOP, QUEEN, KING])
             for (let alignment of [PLAYER, COMPUTER])
                 this.load.image(rank + alignment, rank + alignment + ".png");
     }
+
     create() {
+
         this.add.image(512, 384, "background");
         //         this.add.image(512, 350, "star").setDepth(100);
         //         this.add
@@ -42,40 +39,51 @@ export class Game extends Scene {
         //             })
         //             .setOrigin(0.5)
         //             .setDepth(100);
-        // 4 new files in src/game-objects
-        // 6 sets of chess pieces (3 pairs of BW) in pubic/assets/drummyfish chess; Brought to you by Hope!
+
+
+
         // and a board, and an icon, and a black tile, and a white tile; Totaling to 40 images
-        new ChessTiles(this);
+new ChessTiles(this);
+
         const endButton = this.add.text(100, 100, "End Game!", {
             fill: CREAMHEX,
             backgroundColor: ONYXHEX,
             padding: { left: 20, right: 20, top: 10, bottom: 10 },
         });
-        endButton.setPosition(425, 600);
+        endButton.setPosition(1000, 708);
         endButton.setInteractive();
+
+        // endButton.on("pointerdown", () => {
+        //     this.scene.start("GameOver"); // Reset to original size
+        // });
+
         endButton.on(
             "pointerdown",
             function () {
-                import("./Start") // Dynamically import the Game scene
+                
+                import("./GameOver") // 
                     .then((module) => {
                         // Only add the scene if it's not already registered
-                        if (!this.scene.get("Game")) {
-                            this.scene.add("Game", module.Game); // Add the MainGame scene dynamically
+                        if (!this.scene.get("GameOver")) {
+                            this.scene.add("GameOver", module.GameOver); // Add the MainGame scene dynamically
                         }
-                        // Start the MainGame scene
-                        this.scene.start("Game");
+ // Start the MainGame scene
+                        this.scene.start("GameOver");
                     });
             },
             this
         );
+
         // When the pointer hovers over the button, scale it up
         endButton.on("pointerover", () => {
             endButton.setScale(1.2); // Increase the scale (grow the button by 20%)
         });
+
         // When the pointer moves away from the button, reset the scale to normal
         endButton.on("pointerout", () => {
             endButton.setScale(1); // Reset to original size
         });
+
 
 
         const settingsButton = this.add.text(100, 100, "See Settings", {
@@ -83,6 +91,14 @@ export class Game extends Scene {
             backgroundColor: ONYXHEX,
             padding: { left: 20, right: 20, top: 10, bottom: 10 },
         });
+
+        const rulesButton = this.add.text(100, 100, "See Rules", {
+
+            fill: CREAMHEX,
+            backgroundColor: ONYXHEX,
+            padding: { left: 20, right: 20, top: 10, bottom: 10 },
+        });
+
         settingsButton.setPosition(950, 60);
         settingsButton.setInteractive();
         settingsButton.setOrigin(0.5);
@@ -94,6 +110,23 @@ export class Game extends Scene {
         // When the pointer moves away from the button, reset the scale to normal
         settingsButton.on("pointerout", () => {
             settingsButton.setScale(1); // Reset to original size
+        });
+
+
+
+        rulesButton.setPosition(950, 600);
+        rulesButton.setInteractive();
+   rulesButton.setOrigin(0.5);
+        rulesButton.on("pointerdown", new RulesButton(this).click, this);
+
+        // When the pointer hovers over the button, scale it up
+        rulesButton.on("pointerover", () => {
+            rulesButton.setScale(1.2); // Increase the scale (grow the button by 20%)
+        });
+
+        // When the pointer moves away from the button, reset the scale to normal
+        rulesButton.on("pointerout", () => {
+            rulesButton.setScale(1); // Reset to original size
         });
 
 
