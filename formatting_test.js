2d642ed5d3dc20740ea5daa50550602302c9ff87
Riana Therrien <rtherrie@emich.edu>
2025-03-05 14:29:34 -0500

    Setup for ESLint for code formatting

---
+++ formatting_test.js
@@ -0,0 +1,14 @@
+// EXAMPLE - BEFORE FORMAT
+// const helloYou    = (name)=> {
+//     name = 'you' || name   ;
+//     console.log("hello" + name + "!" )
+//   }
+  
+//   console.log(helloYou)
+
+  const helloYou    = (name)=> {
+    name = 'you' || name   ;
+    console.log("hello" + name + "!" );
+  };
+  
+  console.log(helloYou);
