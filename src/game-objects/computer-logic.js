dc9ec829cf7f3c9fd425b2caee548aad663deade
Marley Higbee <mhigbee1@emich.edu>
2025-03-10 15:20:03 -0400

    made use of dim2Array function in constants to initialize board arrays

--- src/game-objects/computer-logic.js
+++ src/game-objects/computer-logic.js
@@ -1,6 +1,9 @@
 import { BoardState } from "./board-state";
 import { PLAYER,COMPUTER } from "./constants";
 import { KING,QUEEN,BISHOP,KNIGHT,ROOK,PAWN } from "./constants";
+import {PAWN_VALUE, KNIGHT_VALUE, BISHOP_VALUE, ROOK_VALUE,QUEEN_VALUE, KING_VALUE} from "./constants"; 
+import { CAPTURE_WEIGHT, LOSS_WEIGHT, THREATEN_WEIGHT } from "./constants";
+
 
 const rows = 8;
 const columns = 8;
@@ -10,8 +13,9 @@ console.log(board);
 let moveChain;
 
 class gameState {
-    constructor() {
+    constructor(previous) {
         let board = Array.from({ length: rows }, () => new Array(columns).fill([null,null]));
         // console.log(board);
+        this.previous = previous; // previous move in the search chain
     }
 }
