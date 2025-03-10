15a9d6e5cb1118ccd9c3381547b88a8308027ea2
Marley Higbee <mhigbee1@emich.edu>
2025-03-10 15:23:11 -0400

    forgot to save before previous commit

--- src/game-objects/computer-logic.js
+++ src/game-objects/computer-logic.js
@@ -3,18 +3,18 @@ import { PLAYER,COMPUTER } from "./constants";
 import { KING,QUEEN,BISHOP,KNIGHT,ROOK,PAWN } from "./constants";
 import {PAWN_VALUE, KNIGHT_VALUE, BISHOP_VALUE, ROOK_VALUE,QUEEN_VALUE, KING_VALUE} from "./constants"; 
 import { CAPTURE_WEIGHT, LOSS_WEIGHT, THREATEN_WEIGHT } from "./constants";
-
+import { dim2Array } from "./constants";
 
 const rows = 8;
 const columns = 8;
-let board = Array.from({ length: rows }, () => new Array(columns).fill([null,null]));
+let board = dim2Array(8,8);
 console.log(board);
 
 let moveChain;
 
 class gameState {
     constructor(previous) {
-        let board = Array.from({ length: rows }, () => new Array(columns).fill([null,null]));
+        let board = dim2Array(8,8);
         // console.log(board);
         this.previous = previous; // previous move in the search chain
     }
