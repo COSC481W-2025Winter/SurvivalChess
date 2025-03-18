14a65474d1772b3a314b534ab28d95f435bdffb5
Marley Higbee <mhigbee1@emich.edu>
2025-03-18 13:29:46 -0400

    began evaluation function, saving work for merging main

--- src/game-objects/computer-logic.js
+++ src/game-objects/computer-logic.js
@@ -20,11 +20,16 @@ const LIMIT = 4; // depth of search
 class gameState {
     board;
     pieceCoordinates;
+    // bestMove;
+    bestValue;
+    bestMoveInput;
+    bestMoveOutput;
     
     constructor(boardState) {
         this.board = boardState.#boardState;
         this.pieceCoordinates = boardState.#pieceCoordinates;
-
+        // this.bestMove = null;
+        this.bestValue = 10000; // want lowest possible value
         // for (let i=0; i<8; i++) {
         //     for (let j=0; j<8; j++) {
         //         if (false) {
@@ -37,13 +42,60 @@ class gameState {
     
     getBestMove() {
         for (piece in this.pieceCoordinates[COMPUTER]) {
-
+            for (let rank of [PAWN, ROOK, KNIGHT, BISHOP, QUEEN, KING]) { // for all types of computer pieces
+                for (let coordinates of rank) { // for each computer piece of a given rank
+                    moves = this.board.searchMove(coordinates[0], coordinates[1]);
+                    // all possible moves for a piece
+                    for (move in moves) {
+                        this.evaluateBoard(this.board, (coordinates[0], coordinates[1]), move['xy']); // get score for board
+                    }
+                }
+            }
             // get possible moves for each piece
-            piece.searchMove(piece)
+            // let moves = piece.searchMove(piece)
             // Evaluate board
 
             //
         }
     }
     
+    // determines score of a board, lower is better for computer
+    evaluateBoard(boardState, input, output) {
+        boardState.makeMove(input, output); // make the move
+        let score = 0; // initialize score
+
+        // get total value of player material
+        for (piece in this.pieceCoordinates[PLAYER]) { 
+            for (let rank of [PAWN, ROOK, KNIGHT, BISHOP, QUEEN, KING]) { // for all types of computer pieces
+                for (let count of rank) { // for each player piece of a given rank
+                    switch(rank) {
+                        case PAWN:
+                            score+=PAWN_VALUE;
+                            break;
+                        case ROOK:
+                            score+=ROOK_VALUE;
+                            break;
+                        case BISHOP:
+                            score+=BISHOP_VALUE;
+                            break;
+                        case KNIGHT:
+                            score+=KNIGHT_VALUE;
+                            break;
+                        case QUEEN:
+                            score+=QUEEN_VALUE
+                            break;
+                        case KING:
+                            score+=KING_VALUE
+                    }
+                }
+            }
+        }
+
+        if (score < bestValue) { // if move is better than current best move
+            this.bestValue=score;
+            this.bestMoveInput = input;
+            this.bestMoveOutput = output;
+        }
+}
+
 }
