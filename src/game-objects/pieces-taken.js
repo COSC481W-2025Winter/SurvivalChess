33e6879b752322f54472fc159324e5e58817df2a
Hope Heck <113306534+tinymightyhopester@users.noreply.github.com>
2025-03-18 20:49:00 -0400

    added png for more polished look

--- src/game-objects/pieces-taken.js
+++ src/game-objects/pieces-taken.js
@@ -1,6 +1,6 @@
 //local constants for my "captured pieces" container
 const TILE_SIZE = 52;                       // width & height of each tile
-const X_ANCHOR = 1100 - 3.5 * TILE_SIZE;     // x pixel of leftmost tile
+const X_ANCHOR = 1072 - 3.5 * TILE_SIZE;     // x pixel of leftmost tile
 const Y_ANCHOR = 300 - 3.5 * TILE_SIZE;     // y pixel of topmost tile for captured table
 import { WHITE_TILE_COLOR, BLACK_TILE_COLOR, STAGE_COLOR, CREAMHEX, GREENHEX } from "./constants";
 import { PAWN, ROOK, KNIGHT, BISHOP, QUEEN, KING, START_TEXT_ONE, START_TEXT_TWO} from './constants';
@@ -14,49 +14,72 @@ export class PiecesTaken {
         this.load.setPath("assets");
         // Load Chess piece pngs
         this.load.setPath("assets/ourChessPieces");
-        for (let rank of [PAWN, ROOK, KNIGHT, BISHOP, QUEEN, KING])
+        for (let rank of [PAWN, ROOK, KNIGHT, BISHOP, QUEEN])
             for (let alignment of [PLAYER, COMPUTER])
                 this.load.image(rank + alignment, rank + alignment + "4.png");
+
+        
+                
     }
+
+    
 
     scene;
     piecesTaken;
+
+    wPawnScore;
+    bPawnScore;
+    wRookScore;
+    bRookScore;
+    wKnightScore;
+    bKnightScore;
+    wBishopScore;
+    bBishopScore;
+    wQueenScore;
+    bQueenScore;
 
     constructor (scene) {
 
         //set up variables to keep track of elements
         this.scene = scene;
         this.piecesTaken = [];
+        this.wPawnScore = 0;
+        this.bPawnScore = 0;
+        this.wRookScore = 0;
+        this.bRookScore = 0;
+        this.wKnightScore = 0;
+        this.bKnightScore = 0;
+        this.wBishopScore = 0;
+        this.bBishopScore = 0;
+        this.wQueenScore = 0;
+        this.bQueenScore = 0;
 
         this.scene.add
-            .text(930, 18, "Captured Pieces:", {
+            .text(930, 26, "Captured Pieces:", {
                 fontFamily: "'Sans', sans-serif",
                 fontSize: 32,
                 color: START_TEXT_ONE
-            })
-            
-
-
+            });
         this.scene.add.rectangle(
-            1048,
+            1056,
             144,
-            7 * TILE_SIZE,
-            3 * TILE_SIZE,
+            7.5 * TILE_SIZE,
+            2.5 * TILE_SIZE,
             WHITE_TILE_COLOR
         );
         this.scene.add.rectangle(
-            1048,
+            1056,
             144,
-            6 * TILE_SIZE,
-            2 * TILE_SIZE,
+            7.25 * TILE_SIZE,
+            2.25 * TILE_SIZE,
             BLACK_TILE_COLOR
         );
         // Set up captured pieces table
-        for (let i = 0; i < 6; i++) {
+        for (let i = 0; i < 5; i++) {
             this.piecesTaken.push([]);
         }
 
-
+        this.scene.add.image
 
         var j = 0;
         this.addPiece(0, j, PAWN, COMPUTER);
@@ -64,7 +87,7 @@ export class PiecesTaken {
         this.addPiece(2, j, KNIGHT, COMPUTER);
         this.addPiece(3, j, BISHOP, COMPUTER);
         this.addPiece(4, j, QUEEN, COMPUTER);
-        this.addPiece(5, j, KING, COMPUTER);
+        
 
         var j = 1;
         this.addPiece(0, j, PAWN, PLAYER);
@@ -72,7 +95,7 @@ export class PiecesTaken {
         this.addPiece(2, j, KNIGHT, PLAYER);
         this.addPiece(3, j, BISHOP, PLAYER);
         this.addPiece(4, j, QUEEN, PLAYER);
-        this.addPiece(5, j, KING, PLAYER);
+        
 
     }
 
@@ -80,12 +103,14 @@ export class PiecesTaken {
 
         this.piecesTaken[i][j] = new ChessPiece(
             this.scene,
-            X_ANCHOR + i * TILE_SIZE,
+            X_ANCHOR + i * 72,
             Y_ANCHOR + j * TILE_SIZE,
             rank,
             alignment
         );
         this.scene.add.existing(this.piecesTaken[i][j]);
+        this.scene.load.image("x", "x.png");
+        this.scene.add.image(X_ANCHOR +20 + i * 72, Y_ANCHOR + j * TILE_SIZE,"x");
         
     }
 
