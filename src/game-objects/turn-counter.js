61ca83d1d98bfde6b210e4aa3a23aac10f2e4031
moetiba <mtiba2@emich.edu>
2025-04-05 02:08:55 -0400

    rm unnecessary comments

--- src/game-objects/turn-counter.js
+++ src/game-objects/turn-counter.js
@@ -4,20 +4,17 @@ export class TurnCounter {
 		this.scene = scene;
 		this.turnCount = 0;
 
-		// Add the turn counter text element
 		this.turnCounterText = this.scene.add.text(910, 500, `Turn: ${this.turnCount}`, {
 			fontFamily: "'Pixelify Sans', sans-serif",
 			fontSize: 28,
-			color: "#ffffff", // Replace with your color
+			color: "#ffffff",
 		});
 	}
 
-	// Method to update the turn counter display
 	updateTurnCounter() {
 		this.turnCounterText.setText(`Turn: ${this.turnCount}`);
 	}
 
-	// Method to increment the turn counter
 	incrementTurnCounter() {
 		this.turnCount++;
 		this.updateTurnCounter();
