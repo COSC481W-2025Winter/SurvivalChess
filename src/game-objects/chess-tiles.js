887989a8faa97dc3f45fa628821d9d64a409d27f
Marley Higbee <mhigbee1@emich.edu>
2025-04-14 13:36:32 -0400

    fixed issue where if AI took piece before player chose promotion player piece would end up taking computer's piece

--- src/game-objects/chess-tiles.js
+++ src/game-objects/chess-tiles.js
@@ -680,8 +680,10 @@ export class ChessTiles {
 	}
 
 	setPromotion(rank, alignment) {
-		this.boardState.destroyPiece(this.promotionCol, this.promotionRow); // might need update with capture
-		this.boardState.addPiece(this.promotionCol, this.promotionRow, rank, alignment);
+		if (this.boardState.getRank(this.promotionCol, this.promotionRow) == rank) {
+			this.boardState.destroyPiece(this.promotionCol, this.promotionRow); // might need update with capture
+			this.boardState.addPiece(this.promotionCol, this.promotionRow, rank, alignment);
+		}
 	}
 
 	makeComputerMove() {
